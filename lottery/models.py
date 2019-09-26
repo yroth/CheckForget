81a321d53946f6a16d6fe74111f8c742bf438cd2
Gukasyan Liana <lia.gukasyan@gmail.com>
2019-09-26 11:15:09 +0300

    Add realization to condition-10

--- lottery/models.py
+++ lottery/models.py
@@ -401,6 +401,28 @@ class Player(BasePlayer):
     actions_seq_9 = models.StringField()
     actions_seq_10 = models.StringField()
 
+    real_1 = models.IntegerField()
+    real_2 = models.IntegerField()
+    real_3 = models.IntegerField()
+    real_4 = models.IntegerField()
+    real_5 = models.IntegerField()
+    real_6 = models.IntegerField()
+    real_7 = models.IntegerField()
+    real_8 = models.IntegerField()
+    real_9 = models.IntegerField()
+    real_10 = models.IntegerField()
+
+    checked_1 = models.BooleanField()
+    checked_2 = models.BooleanField()
+    checked_3 = models.BooleanField()
+    checked_4 = models.BooleanField()
+    checked_5 = models.BooleanField()
+    checked_6 = models.BooleanField()
+    checked_7 = models.BooleanField()
+    checked_8 = models.BooleanField()
+    checked_9 = models.BooleanField()
+    checked_10 = models.BooleanField()
+
     probA1 = models.StringField()
     probA2 = models.StringField()
     probA3 = models.StringField()
@@ -652,3 +674,93 @@ class Player(BasePlayer):
     realB4_10 = models.StringField()
     probB5_10 = models.StringField()
     realB5_10 = models.StringField()
+
+    def realization_1(self):
+        if (self.choice == 'A'):
+            real_arr = [self.realA1_1, self.realA2_1, self.realA3_1, self.realA4_1, self.realA5_1]
+            prob_arr = [self.probA1_1, self.probA2_1, self.probA3_1, self.probA4_1, self.probA5_1]
+        else:
+            real_arr = [self.realB1_1, self.realB2_1, self.realB3_1, self.realB4_1, self.realB5_1]
+            prob_arr = [self.probB1_1, self.probB2_1, self.probB3_1, self.probB4_1, self.probB5_1]
+        self.real_1 = np.random.choice(real_arr, 1, p=prob_arr)
+
+    def realization_2(self):
+        if (self.choice == 'A'):
+            real_arr = [self.realA1_2, self.realA2_2, self.realA3_2, self.realA4_2, self.realA5_2]
+            prob_arr = [self.probA1_2, self.probA2_2, self.probA3_2, self.probA4_2, self.probA5_2]
+        else:
+            real_arr = [self.realB1_2, self.realB2_2, self.realB3_2, self.realB4_2, self.realB5_2]
+            prob_arr = [self.probB1_2, self.probB2_2, self.probB3_2, self.probB4_2, self.probB5_2]
+        self.real_2 = np.random.choice(real_arr, 1, p=prob_arr)
+
+    def realization_3(self):
+        if (self.choice == 'A'):
+            real_arr = [self.realA1_3, self.realA2_3, self.realA3_3, self.realA4_3, self.realA5_3]
+            prob_arr = [self.probA1_3, self.probA2_3, self.probA3_3, self.probA4_3, self.probA5_3]
+        else:
+            real_arr = [self.realB1_3, self.realB2_3, self.realB3_3, self.realB4_3, self.realB5_3]
+            prob_arr = [self.probB1_3, self.probB2_3, self.probB3_3, self.probB4_3, self.probB5_3]
+        self.real_3 = np.random.choice(real_arr, 1, p=prob_arr)
+
+    def realization_4(self):
+        if (self.choice == 'A'):
+            real_arr = [self.realA1_4, self.realA2_4, self.realA3_4, self.realA4_4, self.realA5_4]
+            prob_arr = [self.probA1_4, self.probA2_4, self.probA3_4, self.probA4_4, self.probA5_4]
+        else:
+            real_arr = [self.realB1_4, self.realB2_4, self.realB3_4, self.realB4_4, self.realB5_4]
+            prob_arr = [self.probB1_4, self.probB2_4, self.probB3_4, self.probB4_4, self.probB5_4]
+        self.real_4 = np.random.choice(real_arr, 1, p=prob_arr)
+
+    def realization_5(self):
+        if (self.choice == 'A'):
+            real_arr = [self.realA1_5, self.realA2_5, self.realA3_5, self.realA4_5, self.realA5_5]
+            prob_arr = [self.probA1_5, self.probA2_5, self.probA3_5, self.probA4_5, self.probA5_5]
+        else:
+            real_arr = [self.realB1_5, self.realB2_5, self.realB3_5, self.realB4_5, self.realB5_5]
+            prob_arr = [self.probB1_5, self.probB2_5, self.probB3_5, self.probB4_5, self.probB5_5]
+        self.real_5 = np.random.choice(real_arr, 1, p=prob_arr)
+
+    def realization_6(self):
+        if (self.choice == 'A'):
+            real_arr = [self.realA1_6, self.realA2_6, self.realA3_6, self.realA4_6, self.realA5_6]
+            prob_arr = [self.probA1_6, self.probA2_6, self.probA3_6, self.probA4_6, self.probA5_6]
+        else:
+            real_arr = [self.realB1_6, self.realB2_6, self.realB3_6, self.realB4_6, self.realB5_6]
+            prob_arr = [self.probB1_6, self.probB2_6, self.probB3_6, self.probB4_6, self.probB5_6]
+        self.real_6 = np.random.choice(real_arr, 1, p=prob_arr)
+
+    def realization_7(self):
+        if (self.choice == 'A'):
+            real_arr = [self.realA1_7, self.realA2_7, self.realA3_7, self.realA4_7, self.realA5_7]
+            prob_arr = [self.probA1_7, self.probA2_7, self.probA3_7, self.probA4_7, self.probA5_7]
+        else:
+            real_arr = [self.realB1_7, self.realB2_7, self.realB3_7, self.realB4_7, self.realB5_7]
+            prob_arr = [self.probB1_7, self.probB2_7, self.probB3_7, self.probB4_7, self.probB5_7]
+        self.real_7 = np.random.choice(real_arr, 1, p=prob_arr)
+
+    def realization_8(self):
+        if (self.choice == 'A'):
+            real_arr = [self.realA1_8, self.realA2_8, self.realA3_8, self.realA4_8, self.realA5_8]
+            prob_arr = [self.probA1_8, self.probA2_8, self.probA3_8, self.probA4_8, self.probA5_8]
+        else:
+            real_arr = [self.realB1_8, self.realB2_8, self.realB3_8, self.realB4_8, self.realB5_8]
+            prob_arr = [self.probB1_8, self.probB2_8, self.probB3_8, self.probB4_8, self.probB5_8]
+        self.real_8 = np.random.choice(real_arr, 1, p=prob_arr)
+
+    def realization_9(self):
+        if (self.choice == 'A'):
+            real_arr = [self.realA1_9, self.realA2_9, self.realA3_9, self.realA4_9, self.realA5_9]
+            prob_arr = [self.probA1_9, self.probA2_9, self.probA3_9, self.probA4_9, self.probA5_9]
+        else:
+            real_arr = [self.realB1_9, self.realB2_9, self.realB3_9, self.realB4_9, self.realB5_9]
+            prob_arr = [self.probB1_9, self.probB2_9, self.probB3_9, self.probB4_9, self.probB5_9]
+        self.real_9 = np.random.choice(real_arr, 1, p=prob_arr)
+
+    def realization_10(self):
+        if (self.choice == 'A'):
+            real_arr = [self.realA1_10, self.realA2_10, self.realA3_10, self.realA4_10, self.realA5_10]
+            prob_arr = [self.probA1_10, self.probA2_10, self.probA3_10, self.probA4_10, self.probA5_10]
+        else:
+            real_arr = [self.realB1_10, self.realB2_10, self.realB3_10, self.realB4_10, self.realB5_10]
+            prob_arr = [self.probB1_10, self.probB2_10, self.probB3_10, self.probB4_10, self.probB5_10]
+        self.real_10 = np.random.choice(real_arr, 1, p=prob_arr)
--- lottery/pages.py
+++ lottery/pages.py
@@ -9,14 +9,18 @@ class Initial(Page):
   form_model = 'player'
   form_fields = ['age', 'gender']
 
+
 class Condition1(Page):
   form_model = 'player'
   form_fields = ['choice', 'actions_seq']
+
   def is_displayed(self):
     return self.round_number <= (Constants.rows_per_condition)
-  def before_next_page(self):   
+
+  def before_next_page(self):
     self.player.checked = 'C' in self.player.actions_seq
     self.player.realization()
+
 
 class Condition10(Page):
   form_model = 'player'
@@ -26,29 +30,59 @@ class Condition10(Page):
                   'actions_seq_1', 'actions_seq_2', 'actions_seq_3',\
                   'actions_seq_4', 'actions_seq_5', 'actions_seq_6',\
                   'actions_seq_7', 'actions_seq_8', 'actions_seq_9', 'actions_seq_10']
+  
   def is_displayed(self):
     return self.round_number > (Constants.rows_per_condition)
+  
   def vars_for_template(self):
     return dict(
       question_number = self.round_number - Constants.rows_per_condition,
       total_questions_number = int(Constants.rows_per_condition / 10),
     )
+  
+  def before_next_page(self):
+    self.player.checked_1 = 'C' in self.player.actions_seq_1
+    self.player.checked_2 = 'C' in self.player.actions_seq_2
+    self.player.checked_3 = 'C' in self.player.actions_seq_3
+    self.player.checked_4 = 'C' in self.player.actions_seq_4
+    self.player.checked_5 = 'C' in self.player.actions_seq_5
+    self.player.checked_6 = 'C' in self.player.actions_seq_6
+    self.player.checked_7 = 'C' in self.player.actions_seq_7
+    self.player.checked_8 = 'C' in self.player.actions_seq_8
+    self.player.checked_9 = 'C' in self.player.actions_seq_9
+    self.player.checked_10 = 'C' in self.player.actions_seq_10
+    self.player.realization_1()
+    self.player.realization_2()
+    self.player.realization_3()
+    self.player.realization_4()
+    self.player.realization_5()
+    self.player.realization_6()
+    self.player.realization_7()
+    self.player.realization_8()
+    self.player.realization_9()
+    self.player.realization_10()
+
 
 class BetweenConditions(Page):
   def is_displayed(self):
     return self.round_number == (Constants.rows_per_condition + 1)
 
+
 class ShortQuestionnarie(Page):
   form_model = 'player'
   form_fields = ['best_strategy_opinion','choose_better_strategy']
+  
   def is_displayed(self):
     return self.round_number == Constants.num_rounds
+  
   def before_next_page(self):
     self.player.get_payoff(self.participant.vars['paying_round'])
+
 
 class EndGame(Page):
   def is_displayed(self):
     return self.round_number == Constants.num_rounds
+  
   def vars_for_template(self):
     paying_round = self.participant.vars['paying_round']
     player = self.player.in_round(paying_round)
